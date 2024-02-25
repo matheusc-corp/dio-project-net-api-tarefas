30c5b4d41a124d7c2e289264e3dee4a12c47dea7
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2024-02-24 22:42:34 -0300

    feat: Incluida classe services para tratar das interacoes da regra de negocios da aplicacao

---
+++ ApiTarefas/Services/TarefaService.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dio_project_net_api_tarefas.Context;
+using dio_project_net_api_tarefas.Dto;
+using dio_project_net_api_tarefas.Models;
+using dio_project_net_api_tarefas.Models.Erros;
+
+namespace dio_project_net_api_tarefas.Services
+{
+    public class TarefaService
+    {
+        public TarefaService(TarefasContext context){
+            _context = context;
+        }
+
+        private TarefasContext _context;
+
+        public List<Tarefa> ListarTarefas(){
+            return _context.Tarefas.ToList();
+        }
+
+        public Tarefa Incluir(TarefaDto tarefaDto){
+            
+            if(string.IsNullOrEmpty(tarefaDto.Titulo))
+                throw new TarefaError("O titulo da tarefa nao pode ser vazio");
+
+            var tarefa = new Tarefa{
+                Titulo = tarefaDto.Titulo,
+                Descricao = tarefaDto.Descricao,
+                Concluida = tarefaDto.Concluida
+            };
+
+            _context.Tarefas.Add(tarefa);
+            _context.SaveChanges();
+            return tarefa;
+        }
+
+        public Tarefa Alterar(TarefaDto tarefa, int id){
+            var tarefaBanco = _context.Tarefas.Find(id);
+
+            if(string.IsNullOrEmpty(tarefa.Titulo))
+                throw new TarefaError("O titulo da tarefa nao pode ser vazio");
+
+            if(tarefaBanco == null){
+                throw new TarefaError("Tarefa nao encontrada");
+            }
+            
+            tarefaBanco.Titulo = tarefa.Titulo;
+            tarefaBanco.Descricao = tarefa.Descricao;
+            tarefaBanco.Concluida = tarefa.Concluida;
+
+            _context.Tarefas.Update(tarefaBanco);
+            _context.SaveChanges();
+
+            return tarefaBanco;
+        }
+
+        public Tarefa BuscaPorId(int id){
+            var tarefaBanco = _context.Tarefas.Find(id);
+
+            if(tarefaBanco == null){
+                throw new TarefaError("Tarefa nao encontrada");
+            }
+
+            return tarefaBanco;
+        }
+
+        public void Deletar(int id){
+            var tarefaBanco = _context.Tarefas.Find(id);
+
+            if(tarefaBanco == null){
+                throw new TarefaError("Tarefa nao encontrada");
+            }
+
+            _context.Tarefas.Remove(tarefaBanco);
+            _context.SaveChanges();
+        }
+        
+    }
+}
