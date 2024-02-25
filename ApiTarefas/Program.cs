34d8afb6145ea62a731ef370dd455c96a613cd16
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2024-02-24 22:45:45 -0300

    fix: Incluido o scoped da classe TarefaSercice para permitir a injecao de dependencia

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -1,14 +1,17 @@
 using dio_project_net_api_tarefas.Context;
+using dio_project_net_api_tarefas.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<TarefaService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TarefasContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+
 
 var app = builder.Build();
 
